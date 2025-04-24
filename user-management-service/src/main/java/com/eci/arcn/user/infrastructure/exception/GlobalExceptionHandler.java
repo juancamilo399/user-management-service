c9d4e073c38a18f81be8b4da041341c992c76812
Juan Sebastián Barrera Pulido <sebasbarrera981119@gmail.com>
2025-04-24 03:46:59 +0000

    Se agrega el dockerfile para contenerizar y se maneja excepción de email ya registrado

---
+++ user-management-service/src/main/java/com/eci/arcn/user/infrastructure/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.eci.arcn.user.infrastructure.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
